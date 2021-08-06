0d1d02bcaf44479b216cb4a47b14f0b3c0a703ca
sheharyar <sheharyarkhalid0313@gmail.com>
2021-08-06 12:22:00 +0500

    initail

---
+++ script/custom.js
@@ -0,0 +1,21 @@
+
+$('.open_close_nav').click(function () {
+    $('.responsive_nav_bar').toggleClass('responsive_bar');
+});
+
+
+
+$('.responsive_service_box').click(function () {
+    $('.service_hover_box').toggleClass('responsive_service_toggle');
+});
+
+$('.responsive_industries_box').click(function () {
+    $('.industries_box').toggleClass('responsive_industry_toggle');
+});
+$('.Content_Hub').click(function () {
+    $('.content_hub_mobile').toggleClass('responsive_industry_toggle');
+});
+
+$('.Company_Hub').click(function () {
+    $('.company_hub_mobile').toggleClass('responsive_industry_toggle');
+});
